1fd60ef99ab0ce833c9b9879c36e31716be0deb6
Gabriel <mutatisguga@gmail.com>
2015-05-21 14:29:07 -0300

    Fome
    Mecanica da fome ficar gorda

--- Assets/Script/Player/Attack/Fome/Mordida.cs
+++ Assets/Script/Player/Attack/Fome/Mordida.cs
@@ -6,23 +6,48 @@ public class Mordida : MonoBehaviour
 	//bool attack;
 	public PolygonCollider2D box;
 
+	public SpriteRenderer sprite;
+
+	public Rigidbody2D rig;
+
+	public float gorduraMax;
+
+	float comeu;
+	float gorduraMin;
+
 	// Use this for initialization
 	void Start () 
 	{
-	
+		comeu = rig.mass;
+		gorduraMin = rig.mass;
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
 		transform.position = PlayerAll.playerTrans.transform.position;
+		if(comeu <= gorduraMax)
+		{
+			rig.mass = comeu;
+		}
+		else
+		{
+			StartCoroutine("Emagrece");
+		}
+	}
+
+	IEnumerator Emagrece()
+	{
+		sprite.color = Color.gray;
+		yield return new WaitForSeconds(2);
+		rig.mass = gorduraMin;
+		sprite.color = Color.white;
+		comeu = gorduraMin;
 	}
 
 	public void Attack()
 	{
-		//attack = true;
 		StopCoroutine("GO");
-		//box.enabled = true;
 		StartCoroutine("GO");
 	}
 
@@ -30,23 +55,22 @@ public class Mordida : MonoBehaviour
 	{
 		yield return new WaitForSeconds (0.7f);
 		box.enabled = false;
-		//attack = false;
 	}
 
 	void OnCollisionEnter2D(Collision2D collision)
 	{
 		if(collision.gameObject.tag == "Enemy")
 		{
-			//if(attack)
 			Destroy(collision.gameObject);
+			comeu += 0.1f;
 		}
 	}
 	void OnCollisionStay2D(Collision2D collision)
 	{
 		if(collision.gameObject.tag == "Enemy")
 		{
-		//	if(attack)
-				Destroy(collision.gameObject);
+			Destroy(collision.gameObject);
+			comeu += 0.1f;
 		}
 	}
 	
@@ -54,8 +78,8 @@ public class Mordida : MonoBehaviour
 	{
 		if(collision.gameObject.tag == "Enemy")
 		{
-		//	if(attack)
 			Destroy(collision.gameObject);
+			comeu += 0.1f;
 		}
 	}
 	
@@ -63,8 +87,8 @@ public class Mordida : MonoBehaviour
 	{
 		if(collision.gameObject.tag == "Enemy")
 		{
-		//	if(attack)
 			Destroy(collision.gameObject);
+			comeu += 0.1f;
 		}
 	}
 }
