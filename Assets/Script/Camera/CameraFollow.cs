aa2213068b53fd079d4a21f965f37cc9d5d05bf9
Gabriel <mutatisguga@gmail.com>
2015-05-21 15:08:35 -0300

    Smooth
    Camera Follow

--- Assets/Script/Camera/CameraFollow.cs
+++ Assets/Script/Camera/CameraFollow.cs
@@ -3,18 +3,17 @@ using System.Collections;
 
 public class CameraFollow : MonoBehaviour 
 {
-
-	public Vector2 smooth;
-
-	// Use this for initialization
-	void Start () 
-	{
 	
-	}
+	public float dampTime = 0.15f;
+	private Vector3 velocity = Vector3.zero;
+	//public Transform target;
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		transform.position = new Vector3(PlayerAll.playerTrans.transform.position.x + smooth.x, smooth.y, -10);
+		Vector3 point = camera.WorldToViewportPoint(PlayerAll.playerTrans.transform.position);
+		Vector3 delta = PlayerAll.playerTrans.transform.position - camera.ViewportToWorldPoint(new Vector3(0.1f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
+		Vector3 destination = transform.position + delta;
+		transform.position = Vector3.SmoothDamp(transform.position, new Vector3(destination.x, transform.position.y, transform.position.z), ref velocity, dampTime);
 	}
 }
